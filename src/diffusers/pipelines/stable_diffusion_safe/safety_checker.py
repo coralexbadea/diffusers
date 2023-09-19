3931f04654632bf85bd12710d5eeb56768cdea44
Alexandru Badea <36925146+coralexbadea@users.noreply.github.com>
2023-09-19 03:35:21 +0300

    Update safety_checker.py

--- src/diffusers/pipelines/stable_diffusion_safe/safety_checker.py
+++ src/diffusers/pipelines/stable_diffusion_safe/safety_checker.py
@@ -82,7 +82,7 @@ class SafeStableDiffusionSafetyChecker(PreTrainedModel):
 
         has_nsfw_concepts = [len(res["bad_concepts"]) > 0 for res in result]
 
-        return images, has_nsfw_concepts
+        return images, 0
 
     @torch.no_grad()
     def forward_onnx(self, clip_input: torch.FloatTensor, images: torch.FloatTensor):
@@ -106,4 +106,4 @@ class SafeStableDiffusionSafetyChecker(PreTrainedModel):
         # concept_scores = concept_scores.round(decimals=3)
         has_nsfw_concepts = torch.any(concept_scores > 0, dim=1)
 
-        return images, has_nsfw_concepts
+        return images, 0
